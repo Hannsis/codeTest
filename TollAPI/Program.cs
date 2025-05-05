8bd61697a318dc093bfeb966f9d9db29eef49693
Hannsis <104864341+Hannsis@users.noreply.github.com>
2025-05-05 21:43:47 +0200

    made the class of weatherstuff an instance, dependancy in constructors.. in prog added svc (#1)
    Co-authored-by: Madsen <hanna.madsen@stena.com>

--- TollAPI/Program.cs
+++ TollAPI/Program.cs
@@ -1,8 +1,17 @@
+using TollAPI.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>(); 
+// if i reference hte interface i must reference the concrete type inside constructor( which is being used ) the DI cointainer
+// it will be injected properly
+//chatGPT can tell you differences. 
+// builder.Services.AddSingleton();
+// builder.Services.AddTransient();
+
 
 var app = builder.Build();
 
