2505981173625feafeec97ddbe3d3183dce68b51
Hannsis <hannamymadsen89@gmail.com>
2025-05-16 11:18:34 +0200

    move toll test folder, that is why the dependancies have been so confusing, and some basic starting tests

---
+++ TollFee.Api.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using TollAPI.Models;
+using TollAPI.Services;
+using Xunit; 
+using Moq;
+
+
+namespace TollFee.Api.Tests;
+
+    public class TollCalculatorTests
+    {
+
+        [Fact]
+        public void GetTollFee_SinglePass_ReturnsSingleFee()
+        {
+            // Arrange
+            // Create a mock of the abstract/interface Vehicle
+          
+            // Set up its IsTollFree property to false:
+
+            // Act
+
+            // Assert
+
+        }
+
+    }
+
