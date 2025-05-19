392ad49ce3b20c7ec37968f3f43edc6232f693ea
Hannsis <hannamymadsen89@gmail.com>
2025-05-19 08:45:13 +0200

    testing right at the timeboundaries

--- TollFee.Api.Tests/UnitTest1.cs
+++ TollFee.Api.Tests/UnitTest1.cs
@@ -21,6 +21,19 @@ namespace TollFee.Api.Tests
                 d.Fee
             });
         }
+
+                public static IEnumerable<object[]> TollBoundaries()
+        {
+            var jsonPath = Path.Combine(AppContext.BaseDirectory, "TestData", "TollBoundaries.json");
+            var json = File.ReadAllText(jsonPath);
+            var docs = JsonConvert.DeserializeObject<List<TimeEntry>>(json);
+
+            return docs.Select(d => new object[]
+            {
+                new DateTime(2013, d.Month, d.Day, d.Hour, d.Minute, 0),
+                d.Fee
+            });
+        }
         public static IEnumerable<object[]> TestTimes()
         {
             var jsonPath = Path.Combine(AppContext.BaseDirectory, "TestData", "SingelPassTest.json");
@@ -75,6 +88,22 @@ namespace TollFee.Api.Tests
         [Theory]
         [MemberData(nameof(TestTimes))]
         public void GetTollFee_SinglePass_Billable_ReturnsFee(DateTime time, int expectedFee)
+        {
+            // Arrange
+            var vehicle = new Mock<Vehicle>();
+            vehicle.SetupGet(v => v.IsTollFree).Returns(false);
+            var calculator = new TollCalculator();
+
+            // Act
+            var total = calculator.GetTollFee(vehicle.Object, new[] { time });
+
+            // Assert
+            Assert.Equal(expectedFee, total);
+        }
+
+        [Theory]
+        [MemberData(nameof(TollBoundaries))]
+        public void GetTollFee_SinglePass_AtEachBoundary_ReturnsCorrectFee (DateTime time, int expectedFee)
         {
             // Arrange
             var vehicle = new Mock<Vehicle>();
@@ -182,7 +211,6 @@ namespace TollFee.Api.Tests
         }
 
         // GetTollFee_SinglePass_BeforeChargeWindow_ReturnsZero         Pass at e.g. 05:59 on a weekday; expect 0.
-        // GetTollFee_SinglePass_AtEachBoundary_ReturnsCorrectFee        tex. 06:00 → 8, 06:29 → 8, 06:30 → 13
  
         // Provide dates out of order but within 60 min; verify correct highest‐fee logic.
         // dates out of order in general 
